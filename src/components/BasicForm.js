c17b4474a1aeb533fc24bbf04e13a12b5d4b1bdc
lytvyn-dev <frontend.lytvyn@gmail.com>
2023-04-06 23:25:32 +0300

    formValidation

--- src/components/BasicForm.js
+++ src/components/BasicForm.js
@@ -1,22 +1,95 @@
+import useInput from "../hooks/use-input";
+
 const BasicForm = (props) => {
+  const {
+    inputValue: firstNameValue,
+    entredInputIsValid: firsNameIsValid,
+    hasError: firstNameHasError,
+    inputBlurHandler: blurFirstName,
+    inputChangeHandler: firstNameChange,
+    reset: resetFirstName,
+  } = useInput((value) => value.trim() !== "");
+
+  const {
+    inputValue: lastNameValue,
+    entredInputIsValid: lastNameIsValid,
+    hasError: lastNameHasError,
+    inputBlurHandler: blurLastName,
+    inputChangeHandler: lastNameChange,
+    reset: resetLastName,
+  } = useInput((value) => value.trim() !== "");
+
+  const {
+    inputValue: emailValue,
+    entredInputIsValid: emailIsValid,
+    hasError: emailHasError,
+    inputBlurHandler: blurEmail,
+    inputChangeHandler: emailChange,
+    reset: resetEmail,
+  } = useInput((value) => value.includes("@"));
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+    resetFirstName();
+    resetLastName();
+    resetEmail();
+  };
+
+  //? Form handling
+  let formIsInvalid = true;
+
+  if (emailIsValid && lastNameIsValid && firsNameIsValid) {
+    formIsInvalid = false;
+  }
+
+  //? Dynamic styles change hadling
+
+  const fistNameValid = firstNameHasError ? "form-control invalid" : "form-control";
+  const lastNameValid = lastNameHasError ? "form-control invalid" : "form-control";
+  const emailValid = emailHasError ? "form-control invalid" : "form-control";
+
   return (
-    <form>
-      <div className='control-group'>
-        <div className='form-control'>
-          <label htmlFor='name'>First Name</label>
-          <input type='text' id='name' />
+    <form onSubmit={onSubmitHandler}>
+      <div className="control-group">
+        <div className={fistNameValid}>
+          <label htmlFor="fist-name">First Name</label>
+          <input
+            value={firstNameValue}
+            onBlur={blurFirstName}
+            onChange={firstNameChange}
+            type="text"
+            id="fist-name"
+          />
+          {firstNameHasError && <p className="error-text">First name should no an empty string!</p>}
         </div>
-        <div className='form-control'>
-          <label htmlFor='name'>Last Name</label>
-          <input type='text' id='name' />
+
+        <div className={lastNameValid}>
+          <label htmlFor="last-name">Last Name</label>
+          <input
+            value={lastNameValue}
+            onChange={lastNameChange}
+            onBlur={blurLastName}
+            type="text"
+            id="last-name"
+          />
+          {lastNameHasError && <p className="error-text">First name should no an empty string!</p>}
         </div>
       </div>
-      <div className='form-control'>
-        <label htmlFor='name'>E-Mail Address</label>
-        <input type='text' id='name' />
+
+      <div className={emailValid}>
+        <label htmlFor="email">E-Mail Address</label>
+        <input
+          value={emailValue}
+          onChange={emailChange}
+          onBlur={blurEmail}
+          type="email"
+          id="email"
+        />
+        {emailHasError && <p className="error-text">Email field should include @</p>}
       </div>
-      <div className='form-actions'>
-        <button>Submit</button>
+
+      <div className="form-actions">
+        <button disabled={formIsInvalid}>Submit</button>
       </div>
     </form>
   );
